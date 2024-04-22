1f114c138f28c724598756e9ec77e57f65a6bfd0
bobbylee <lenamthanh712@gmail.com>
2024-04-22 18:19:12 +0700

    checkout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Home from "./Home/Home";
 import Register from "./Register/Register";
 import Login from "./Login/Login";
-
+import CheckOut from "./CheckOut/CheckOut";
 import Account from "./Account/Account";
 import ProductCart from "./ProductCart/ProductCart";
 import Orders from "./Orders/Orders";
@@ -11,7 +11,17 @@ import ChainStore from "./ChainStore/ChainStore";
 import Contact from "./Contact/Contact";
 import AllProducts from "./AllProducts/AllProducts";
 
-
-export { Home, AllProducts, Login, Register, Contact, Account, Orders, ChangePass, Address, ChainStore, ProductCart };
-
-
+export {
+  Home,
+  AllProducts,
+  Login,
+  Register,
+  Contact,
+  Account,
+  Orders,
+  ChangePass,
+  Address,
+  ChainStore,
+  ProductCart,
+  CheckOut,
+};
